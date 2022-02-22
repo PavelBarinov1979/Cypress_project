885708e66bacffd9161be2a977a33b5c95c52380
Pavel Barinov <pavel_bv@mail.ru>
2022-02-22 15:57:50 +0300

    1

--- cypress/integration/TestIntefration.js
+++ cypress/integration/TestIntefration.js
@@ -1,2 +1,24 @@
-import { mobileReplenishment } from "../support/Pages/MobileReplenishment";
-import { basePage } from "../support/Pages/basePage";
+//import { mobileReplenishment } from "../support/Pages/MobileReplenishment";
+//import { basePage } from "../support/Pages/basePage";
+
+it('Example sending Get request', () => {
+   cy.request('https://next.privat24.ua')
+      .then((response)=>{
+           console.log(responce); 
+      })
+});
+
+it('Example sending Post request', () => {
+    cy.request({
+         method:
+         url:
+         body:
+         headers:
+
+
+
+    })
+       .then((response)=>{
+            console.log(responce); 
+       })
+ });
